ca3086632edfc04905f2fb1f0cc7982311608d90
Luis Gustavo <luis.gustavo.freitas.a@gmail.com>
2025-06-03 19:00:19 -0300

    fix wrong word on urls

--- apps/bibliotecarios/urls.py
+++ apps/bibliotecarios/urls.py
@@ -6,6 +6,6 @@ app_name = 'bibliotecarios'
 urlpatterns = [
     path('adicionar/', views.add_bibliotecario, name='add_bibliotecario'),
     path('', views.list_bibliotecarios, name='list_bibliotecarios'),
-    path('editar/<int:id_bibliotecario>/', views.edit_bibliotecarios, name='edit_bibliotecarios'),
-    path('excluir/<int:id_bibliotecario>/', views.delete_bibliotecarios, name='delete_bibliotecarios'),
+    path('editar/<int:id_bibliotecario>/', views.edit_bibliotecario, name='edit_bibliotecario'),
+    path('excluir/<int:id_bibliotecario>/', views.delete_bibliotecario, name='delete_bibliotecario'),
 ]
