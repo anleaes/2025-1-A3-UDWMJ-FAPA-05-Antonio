7f0a46622a7ff37db00dccd3b28161020a2b2e48
Luis Gustavo de Freitas Antunes - 12924111583 <12924111583@ulife.com.br>
2025-06-04 09:25:15 -0300

    add forms and finish views

--- apps/categorias/views.py
+++ apps/categorias/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .forms import ClienteForm
+from .forms import CategoriaForm
 from .models import Categoria
 
 # Create your views here.
