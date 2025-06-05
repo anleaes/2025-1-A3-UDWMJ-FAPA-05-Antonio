9c6b66412857182b4eddf3c74cb2ed449f3f1ab8
Luis Gustavo <luis.gustavo.freitas.a@gmail.com>
2025-06-05 16:09:13 -0300

    remove some fields from emprestimos, fix migration, fix list emprestimos

--- bibliotecaapp/settings.py
+++ bibliotecaapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'core.apps.CoreConfig', 
     'clientes.apps.ClientesConfig', 
     'bibliotecarios.apps.BibliotecariosConfig', 
